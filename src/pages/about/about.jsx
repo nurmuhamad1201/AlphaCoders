e9723b3743a1df70ad3276bc9146bc3579aac6fd
abdulaziz <abulazizdavlatov@gmail.com>
2024-10-19 18:58:13 +0500

    abdu1

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import img from "../../assets/logo//photo_2024-10-19 16.33.32.jpeg"
+import img from "../../assets/img/photo_2024-10-19 16.33.32.jpeg"
+import img1 from "../../assets/img/canun.jpg"
+import img2 from "../../assets/img/orlando.jpeg"
+import img3 from "../../assets/img/los_angeles.jpeg"
+import img4 from "../../assets/img/puerto.jpeg"
+import img5 from "../../assets/img/miami.jpeg"
 
 const About = () => {
   return (
@@ -81,7 +86,7 @@ const About = () => {
 
 <div>
 <img className='h-[550px] px-[20px] rounded-[10px]' src={img} alt="" />
-<button className='absolute mt-[-120px] ml-[180px] w-[300px] h-[40px] border-[1px] border-[solid] border-[blue] text-[white] rounded-[5px] bg-[blue] shadow-[0px_0px_50px_white]'>Explore Deals in map</button>
+<button className='absolute mt-[-120px] ml-[180px] w-[300px] h-[40px] border-[1px] border-[solid] border-[blue] text-[white] rounded-[10px] bg-[blue] shadow-[0px_0px_50px_white]'>Explore Deals in map</button>
 </div>
 
 </div>
@@ -89,10 +94,77 @@ const About = () => {
 
 
 <div>
-  <h1 className='text-[40px] '>
+  <h1 className='text-[40px] m-[50px]'>
   Trending Destinationss
   </h1>
 </div>
+
+
+
+
+<div className='flex px-[30px] py-[30px]'> 
+<div className='flex w-[1050px] flex-wrap gap-[10px]'>
+
+
+<div>
+  <img className='w-[450px] h-[350px]' src={img1} alt="" />
+  <div className='absolute mt-[-230px] ml-[150px] text-[center] text-[white]'>
+    <h1 className='text-[40px]'>Canun</h1>
+    <p>Canun Beach</p>
+  </div>
+</div>
+
+
+<div>
+  <img className='w-[450px] h-[350px]' src={img3} alt="" />
+  <div className='absolute mt-[-240px] ml-[150px] text-[center] text-[white]'>
+    <h1 className='text-[40px]'>Los Angeles</h1>
+    <p className='ml-[50px]'>Downtown</p>
+  </div>
+</div>
+
+<div>
+  <img className='w-[450px] h-[350px]' src={img2} alt="" />
+  <div className='absolute mt-[-200px] ml-[150px] text-[center] text-[white]'>
+    <h1 className='text-[40px]'>Orlando</h1>
+    <p>Orlando Eye</p>
+  </div>
+</div>
+
+
+<div>
+  <img className='w-[450px] h-[350px]' src={img4} alt="" />
+  <div className='absolute mt-[-220px] ml-[130px] text-[center] text-[white]'>
+    <h1 className='text-[40px]'>Puerto Valarta</h1>
+    <p>Majahuista Beach</p>
+  </div>
+</div>
+
+</div>
+
+<div>
+  <img className='w-[500px] h-[700px]' src={img5} alt="" />
+  <div className='absolute mt-[-400px] ml-[200px] text-[center] text-[white]'>
+    <h1 className='text-[40px]'>Miami</h1>
+    <p>South Beach</p>
+  </div>
+</div>
+
+</div>
+
+
+<div>
+  <h1 className='text-[40px] m-[50px]'>
+  Featured Deals*
+  </h1>
+</div>
+
+
+<div>
+  <img src="" alt="" />
+</div>
+
+
 
     </div>
   )
