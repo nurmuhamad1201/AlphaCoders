d2126dde42f5b5da9c934adf910814087f3338b9
abdulaziz <abulazizdavlatov@gmail.com>
2024-10-20 16:05:42 +0500

    gift

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -103,72 +103,73 @@ const Home = () => {
   
   return (
     <div>
-      <div className="relative h-[400px]">
-        <img className="w-full h-full object-cover" src={istiqlol} alt="" />
+      <div className="relative h-[740px]">
+        <img className="w-full h-[740px] object-cover" src={istiqlol} alt="" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+        <h1 className="text-4xl font-bold">Hello</h1>
           <h1 className="text-4xl font-bold">Where do you want to explore?</h1>
           <p>{`Current Location: ${currentLocation}`}</p>
         </div>
       </div>
 
-      <div className="  w-full p-8 bg-white shadow-lg">
-        <Box width='80%' margin="auto" display="flex" alignItems="center" gap={2}>
-          <FormControl fullWidth>
-            <InputLabel>Current Location</InputLabel>
-            <Select
-              value={currentLocation}
-              onChange={(e) => setCurrentLocation(e.target.value)}
-              label="Current Location"
-            >
-              <MenuItem value="">Select your current location</MenuItem>
-              <MenuItem value={currentLocation}>{currentLocation}</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl fullWidth>
-            <InputLabel>Select City</InputLabel>
-            <Select
-              value={selectedCity}
-              onChange={handleCityChange}
-              label="Select City"
-            >
-              <MenuItem value="">Choose a city</MenuItem>
-              {cityData.map((city) => (
-                <MenuItem key={city.id} value={city.city}>
-                  {city.city}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-          <TextField
-            type="date"
-            label="Select Date"
-            InputLabelProps={{ shrink: true }}
-            value={selectedDate}
-            onChange={handleDateChange}
-            fullWidth
-          />
-
-          <PassengerSelector onUpdateTotal={handlePassengerUpdate} />
-        </Box>
-
-        <Box width='80%' margin="auto" display="flex" justifyContent="space-between" mt={2}>
-          <Button variant="contained" color="primary" sx={{ height: 'fit-content' }}>
-            Contact Us
-          </Button>
-
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{ height: 'fit-content' }}
-            onClick={handleCreateFlight}
+     <div className="w-full p-8 bg-white shadow-lg">
+      <Box width='80%' margin="auto" display="flex" alignItems="center" gap={2}>
+        <FormControl fullWidth>
+          <InputLabel>Current Location</InputLabel>
+          <Select
+            value={currentLocation}
+            onChange={(e) => setCurrentLocation(e.target.value)}
+            label="Current Location"
           >
-            Create Flight
-          </Button>
-        </Box>
-      </div>
+            <MenuItem value="">Select your current location</MenuItem>
+            <MenuItem value={currentLocation}>{currentLocation}</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth>
+          <InputLabel>Select City</InputLabel>
+          <Select
+            value={selectedCity}
+            onChange={handleCityChange}
+            label="Select City"
+          >
+            <MenuItem value="">Choose a city</MenuItem>
+            {cityData.map((city) => (
+              <MenuItem key={city.id} value={city.city}>
+                {city.city}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <TextField
+          type="date"
+          label="Select Date"
+          InputLabelProps={{ shrink: true }}
+          value={selectedDate}
+          onChange={handleDateChange}
+          fullWidth
+        />
+
+        <PassengerSelector onUpdateTotal={handlePassengerUpdate} />
+      </Box>
+
+      <Box width='80%' margin="auto" display="flex" justifyContent="space-between" mt={2}>
+        <Button variant="contained" color="primary" sx={{ height: 'fit-content' }}>
+          Contact Us
+        </Button>
+
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{ height: 'fit-content' }}
+          onClick={handleCreateFlight}
+        >
+          Create Flight
+        </Button>
+      </Box>
+    </div>
 
       <div className="p-4">
        
